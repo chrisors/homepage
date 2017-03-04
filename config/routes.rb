d72d2243dbee22de9a46c377c6b548a0192ab98a
Christian Sørseth <csorseth@gmail.com>
2017-03-04 18:40:04 +0100

    adding about controller, also making a grid with relevant information baout yourself

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'dashboard/index'
   root to: 'dashboard#index'
+
+  get 'about/index'
 end
