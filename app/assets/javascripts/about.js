faa4f226427de74659682601217040576546d926
Christian Sørseth <csorseth@gmail.com>
2017-03-13 19:36:53 +0100

    adding gradient to background image to make font clearer

--- app/assets/javascripts/about.js
+++
@@ -1,8 +0,0 @@
-
-$(document).on('turbolinks:load', function() {
-    var navbar = document.getElementById("navbar");
-    var item = navbar.getElementsByClassName("item")
-    for (var i = 0; i < item.length; i ++) {
-        item[i].style.color = "black";
-    }
-});
